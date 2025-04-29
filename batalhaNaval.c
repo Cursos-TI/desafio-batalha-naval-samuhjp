be275b9d4a464e034f7705962ee6e437c7218d40
sam <samuelpdalvi@gmail.com>
2025-04-29 15:35:46 +0000

    Batalha_Naval

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,45 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+#define LINHAS 11
+#define COLUNAS 9
 
-int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+int main(){
+    char linha[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+    int i, j, tabuleiro[LINHAS][COLUNAS];
+    int soma = 0; 
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
-
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
-
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
+    printf("   TABULEIRO BATALHA NAVAL \n");
     
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+    for (int j = 0; j <= 10; j++){
+        printf("%c  ", linha[j]);
+    }
+    
+        printf("  \n");
 
-    return 0;
+    for (int i = 1; i < LINHAS; i++){
+        printf("%2d  ", i); 
+        for (int j = 0; j < COLUNAS; j++){
+            tabuleiro[i][j] = soma;
+            printf("%d  ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+
+
+    for(i = 0; i <= LINHAS; i++){
+		for (j = 0; j < COLUNAS; j++){
+            
+			if(i + j == LINHAS - 1){
+				tabuleiro[i][j] = 3;
+			} else {
+				tabuleiro[i][j] = 0;
+			} 
+            if(i - j == COLUNAS - 9){
+                tabuleiro[i][j] = 3;
+            }
+		}
+	}
+    
+  return 0;
 }
